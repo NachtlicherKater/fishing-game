996de590375775d2f7628b9f2e18a11a2a6270f4
Dmitry Salashchenko <ho4houkot39@gmail.com>
2025-09-05 04:30:23 +0300

    feat: add 'how to play' button

--- scripts/main-menu.js
+++ scripts/main-menu.js
@@ -1,5 +1,7 @@
 // Buttons
 const startButton = document.querySelector('.start-button');
+const aboutButton = document.querySelector('.about-game-button');
+const aboutCloseButton = document.querySelector('.close-about-game');
 const settingsButton = document.querySelector('.settings-button');
 const leaderboardButton = document.querySelector('.leaderboards-button');
 const leaderboardCloseButton = document.querySelector('.close-leaderboards');
@@ -7,6 +9,7 @@ const exitButton = document.querySelector('.exit-button');
 // Windows
 const mainWindow = document.querySelector('.main-menu');
 const gameWindow = document.querySelector('.game');
+const aboutWindow = document.querySelector('.about-game');
 const leaderboardsWindow = document.querySelector('.leaderboards');
 const settingsWindow = document.querySelector('.settings');
 
@@ -17,7 +20,17 @@ startButton.addEventListener('click', () => {
     mainWindow.classList.remove('show');
     mainWindow.classList.add('hide');
     gameWindow.classList.remove('hide');
-    gameWindow.classList.add('show');
+    gameWindow.classList.add('show-game');
+});
+
+aboutButton.addEventListener('click', () => {
+    aboutWindow.classList.remove('hide');
+    aboutWindow.classList.add('show');
+});
+
+aboutCloseButton.addEventListener('click', () => {
+    aboutWindow.classList.remove('show');
+    aboutWindow.classList.add('hide');
 });
 
 leaderboardButton.addEventListener('click', () => {
